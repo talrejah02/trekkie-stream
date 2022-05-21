dbb36b62ccc97c0a2dfdd794cff2e38b8e842557
talrejah02 <talrejah02@gmail.com>
2022-05-21 16:08:25 +0530

    Refactor:category

--- src/pages/explore/explore.jsx
+++ src/pages/explore/explore.jsx
@@ -1,52 +1,66 @@
-import axios from 'axios'
-import React from 'react'
-import { useState } from "react"
-import { useEffect } from 'react'
-import { Videolisting } from '../../components'
-import { usePlaylist, useVideoscontext } from '../../context'
-import { getFilteredvideos } from '../../utils/getFilteredvideos'
-import { Exploreicon } from '../assets/svg/exploreIcon'
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Videolisting } from "../../components";
+import { usePlaylist, useVideoscontext } from "../../context";
+import { getFilteredvideos } from "../../utils/getFilteredvideos";
+import { Exploreicon } from "../assets/svg/exploreIcon";
 import { Playlistmodal } from "../../components";
 
-import "./explore.css"
+import "./explore.css";
 
 function Explore() {
   const [videoList, setVideolist] = useState([]);
-  const { state } = useVideoscontext();
-  const {showModal}=usePlaylist()
+  const { state,dispatch } = useVideoscontext();
+  const { showModal } = usePlaylist();
   
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get(`/api/videos`)
-        setVideolist(response.data.videos)
+        const response = await axios.get(`/api/videos`);
+        setVideolist(response.data.videos);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     })();
-  }, [])
-  const filteredVideolist = getFilteredvideos(state, videoList)
-  
+  }, []);
+  const filteredVideolist = getFilteredvideos(state, videoList);
+  const categoryHandler = (category) => {
+    dispatch({type:"category",payload:category})
+  }
   return (
-    <div className='main-container'>
-        <section  className='container-header'>
-            <section  className='container-header-content'>
-            <span className='header-icon'>
-                <Exploreicon />
-            </span>
-            <span className='header-text'>Explore</span>
-            </section>
-            <section className='header-search'>
-            <span className='header-icon'>
-                <Exploreicon />
-            </span>
-            <input className='header-search-input' type="text" placeholder='search' />
-            </section>
+    <div className="main-container">
+      <section className="container-header">
+        <section className="container-header-content">
+          <span className="header-icon">
+            <Exploreicon />
+          </span>
+          <span className="header-text">Explore</span>
         </section>
+        <section className="header-search">
+          <span className="header-icon">
+            <Exploreicon />
+          </span>
+          <input
+            className="header-search-input"
+            type="text"
+            placeholder="search"
+          />
+        </section>
+        <section className="chips-container">
+          Category:
+          <span className="category-chip" onClick={(e) => {
+            categoryHandler("Backpacking")
+          }}>Backpacking</span>
+          <span className="category-chip" onClick={()=>categoryHandler("Altitude Sickness")}>Altitude Sickness</span>
+          <span className="category-chip" onClick={()=>categoryHandler("All Trekks")}>All Trekks</span>
+        </section>
+      </section>
       <Videolisting videos={filteredVideolist} type={"explore"} />
       {showModal && <Playlistmodal />}
     </div>
-  )
+  );
 }
 
-export {Explore}
+export { Explore };
